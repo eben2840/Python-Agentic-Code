40f76682b9f3fb90893d7473ab815fbe7faa67f6
eben2840 <ebenmills200@gmail.com>
2026-04-29 16:38:24 +0200

    newpush

--- skills.py
+++ skills.py
@@ -57,7 +57,7 @@ def extract_questionnaire():
         'extraction/extraction_questionnaire.md',
         transcription=transcript,
         itemsDescription=questionnaire,
-    )
+    )   
   
     llm      = ClaudeLLMService()
     response = llm.client.messages.create(
